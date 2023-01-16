d6faa062d33c9424114f3ccd003da530dd1ab0d5
jjaegii <hn06038@gmail.com>
2023-01-16 11:19:13 +0900

    add another rtsp

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-python track.py --source rtsp://brighten:brighten0701@192.168.0.22/stream1 --img 1280
+# python track.py --source rtsp://brighten:brighten0701@192.168.0.22/stream1 --img 1280
+python track.py --source rtsp://brighten:brighten0701@192.168.0.44/stream_ch00_0 --img 1280
