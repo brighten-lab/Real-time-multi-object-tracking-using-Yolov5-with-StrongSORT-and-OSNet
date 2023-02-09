a7c42a906206aaef7819e17207d8db3bbdd2aab4
jjaegii <hn06038@gmail.com>
2023-02-09 11:38:22 +0900

    run track and webserver at the sametime

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-# python track.py --source rtsp://brighten:brighten0701@192.168.0.22/stream1 --img 1280
-python track.py --source rtsp://brighten:brighten0701@192.168.0.44/stream_ch00_0 --img 1280
+python3 track.py --source rtsp://brighten:brighten0701@192.168.0.22/stream1 --img 1280 &
+# python track.py --source rtsp://brighten:brighten0701@192.168.0.44/stream_ch00_0 --img 1280 &
+cd web
+python3 server.py
